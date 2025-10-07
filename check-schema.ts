bda123b82c06bd9d53f002d49db0201684dfed10
jsaddiction <04reduramax@gmail.com>
2025-10-07 06:26:19 -0400

    removed claude shell management

--- check-schema.ts
+++
@@ -1,9 +0,0 @@
-import Database from 'better-sqlite3';
-
-const db = new Database('datametarr.sqlite');
-
-const schema = db.prepare("PRAGMA table_info(movies)").all();
-console.log('Movies table schema:');
-console.log(JSON.stringify(schema, null, 2));
-
-db.close();
